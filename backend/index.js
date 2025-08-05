1e3d1dfdfb8e15280dcec74757387d6db6554ce9
omkarhole05 <holeom191@gmail.com>
2025-08-05 16:37:55 +0530

    `Added CORS configuration with hardcoded origin URL`

--- backend/index.js
+++ backend/index.js
@@ -15,14 +15,19 @@ const URL=process.env.MONGO_URL;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin:"https://deepseek-ai-clone-git-main-holeom191-gmailcoms-projects.vercel.app",
-    credentials:true,
-    methods:['GET','POST','PUT','DELETE'],
-    allowedHeaders:['Content-Type','Authorization']
-}))
+// app.use(cors({
+//     origin:"https://deepseek-ai-clone-git-main-holeom191-gmailcoms-projects.vercel.app",
+//     credentials:true,
+//     methods:['GET','POST','PUT','DELETE'],
+//     allowedHeaders:['Content-Type','Authorization']
+// }))
 
-
+const corsOptions = {
+    origin: "https://deepseek-ai-clone-git-main-holeom191-gmailcoms-projects.vercel.app", // remove trailing slash
+    credentials: true,
+};
+const cors = require("cors");
+app.use(cors(corsOptions));
 //DB connection
 try {
     mongoose.connect(URL)
